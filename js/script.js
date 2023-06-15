f7302f6dae7d5256268b1b1b28ca972d0789c688
knwg-s05 <ratton.giggs@gmail.com>
2023-06-15 13:42:48 +0900

    浮かび上がりコメントアウト

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
-window.addEventListener('scroll', function() {
-  var contentElements = document.querySelectorAll('.content');
+// window.addEventListener('scroll', function() {
+//   var contentElements = document.querySelectorAll('.content');
   
-  for (var i = 0; i < contentElements.length; i++) {
-    var element = contentElements[i];
-    var position = element.getBoundingClientRect();
+//   for (var i = 0; i < contentElements.length; i++) {
+//     var element = contentElements[i];
+//     var position = element.getBoundingClientRect();
     
-    if (position.top < window.innerHeight) {
-      element.classList.add('visible');
-    } else {
-      element.classList.remove('visible');
-    }
-  }
+//     if (position.top < window.innerHeight) {
+//       element.classList.add('visible');
+//     } else {
+//       element.classList.remove('visible');
+//     }
+//   }
+// });
+
+// Page TOPへもどる
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+$('a[href^="#"]').click(function(){
+  var time = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" ? 'html' : href);
+  var distance = target.offset().top;
+  $("html, body").animate({scrollTop:distance}, time, "swing");
+  return false;
+});
 });
